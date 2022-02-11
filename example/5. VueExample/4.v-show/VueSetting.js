d9015657879810d99e9b06a3ef0fafca901c71cc
Akira Chen <man4781747@gmail.com>
2022-02-11 14:18:46 +0800

    add example

---
+++ example/5. VueExample/4.v-show/VueSetting.js
@@ -0,0 +1,6 @@
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    switchNum: 0
+  },
+})
---
+++ example/5. VueExample/5.v-for/VueSetting.js
@@ -0,0 +1,9 @@
+var app5 = new Vue({
+  el: '#app-5',
+	data() {
+	  return {
+	    parentMessage: 'Parent',
+	    items: [{ message: 'Foo' }, { message: 'Bar' }]
+	  }
+	}
+})
